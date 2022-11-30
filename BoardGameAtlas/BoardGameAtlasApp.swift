d268b2cacbf47580872248b240e82983e3d6212d
Florian <florian.jimenez@etu.iut-tlse3.fr>
2022-11-30 14:01:27 +0100

    Initial Commit

---
+++ BoardGameAtlas/BoardGameAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGameAtlasApp.swift
+//  BoardGameAtlas
+//
+//  Created by Florian Jimenez on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BoardGameAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
